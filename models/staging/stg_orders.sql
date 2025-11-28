9dad8822cf7291212d68e5f0191b5eaa5144b289
Sahil Goyal <TrainingUser24@dgmtech.cloud>
2025-11-28 04:40:22 +0000

    commit

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,11 +1,4 @@
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-select
-    order_id,
-    customer_id,
-    ordered_at,
-    status,
-    order_total,
-    currency,
-    updated_at
-from {{ source('raw', 'orders') }}
+select order_id, customer_id, ordered_at, status, order_total, currency, updated_at
+from {{ source("raw", "orders") }}
