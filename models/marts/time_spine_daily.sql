280d904ee9228971f565ebb32037a72fcd2fe3c8
Sahil Goyal <TrainingUser24@dgmtech.cloud>
2025-11-28 05:42:38 +0000

    commit

--- models/marts/time_spine_daily.sql
+++ models/marts/time_spine_daily.sql
@@ -1,25 +1,17 @@
--- Daily time spine for MetricFlow / Semantic Layer
-
-{{ config( materialized = 'table'
-) }}
+-- models/marts/time_spine_daily.sql
+{{ config(materialized='table') }}
 
 with base_dates as (
 
     {{ dbt.date_spine(
         'day',
-        "DATE('2000-01-01')",   
-        "DATE('2030-01-01')"    
+        "DATE('2000-01-01')",
+        "DATE('2030-01-01')"
     ) }}
 
-),
-
-final as (
-    select
-        cast(date_day as date) as date_day
-    from base_dates
 )
 
-select *
-from final
+select cast(date_day as date) as date_day
+from base_dates
 where date_day > dateadd(year, -5, current_date())
-  and date_day < dateadd(day, 30, current_date())
+  and date_day < dateadd(day, 30, current_date());
